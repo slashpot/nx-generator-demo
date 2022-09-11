848a04a03964e81d819414d064b7e5d8080ff9d0
Eric Lin <eric.lin@titansoft.com.sg>
2022-09-11 10:47:35 +0800

    update generator

--- demo-org/tools/generators/demo-generator/index.ts
+++ demo-org/tools/generators/demo-generator/index.ts
@@ -1,11 +1,13 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+//original: import { libraryGenerator } from '@nrwl/workspace/generators';
+import { applicationGenerator } from '@nrwl/next/src/generators/application/application';
+import { Schema } from '@nrwl/next/src/generators/application/schema';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+export default async function (tree: Tree, schema: Schema) {
+  //original: await libraryGenerator(tree, { name: schema.name });
+  await applicationGenerator(tree, schema);
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
   };
 }
-``
